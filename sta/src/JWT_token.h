97df7f8655ef1205d779689af05c8ac38193ee80
Syver <syverharaldsen@gmail.com>
2025-03-26 12:13:27 +0100

    Add instructions to ignore JWT_token.h changes locally

--- sta/src/JWT_token.h
+++ sta/src/JWT_token.h
@@ -1,5 +1,11 @@
-
+/* Remember not to push your JWT to public repo.
+    * You can use the following command to ignore changes to this file locally:
+    * git update-index --assume-unchanged sta/src/JWT_token.h
+    * To start tracking changes again, use:
+    * git update-index --no-assume-unchanged sta/src/JWT_token.h
+*/
 /* Here you define your JWT Authentication token */
+
 // #define AUTH_TOKEN ""
 
 #ifndef AUTH_TOKEN
